8a095392e782d36097da2b61010dfce353fba152
Daniel Marc <danielmarcelo.junior@hotmail.com>
2021-09-19 20:24:06 -0300

    :sparkles: Adiciona métodos de acesso à tabela usuario
    Signed-off-by: Daniel Marc <danielmarcelo.junior@hotmail.com>

--- src/main/java/utils/HibernateUtil.java
+++ src/main/java/utils/HibernateUtil.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 
 import entity.Usuario;
 
