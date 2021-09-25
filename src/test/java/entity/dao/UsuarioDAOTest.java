8f3395b1ac802de31f00c3f12593670ea562388b
Daniel Marc <danielmarcelo.junior@hotmail.com>
2021-09-25 19:15:11 -0300

    :hammer: Altera testes de acesso à tabela Usuario
    Signed-off-by: Daniel Marc <danielmarcelo.junior@hotmail.com>

--- src/test/java/entity/dao/UsuarioDAOTest.java
+++ src/test/java/entity/dao/UsuarioDAOTest.java
@@ -1,107 +1,169 @@
 package entity.dao;
 
-//import static org.junit.Assert.assertNotNull;
-//import static org.junit.Assert.assertNull;
-//import static org.junit.Assert.assertTrue;
-//
-//import java.util.List;
-//
-//import org.hibernate.SessionFactory;
-//import org.junit.FixMethodOrder;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.MethodSorters;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//import entity.Usuario;
-//import utils.HibernateTestUtils;
-//import utils.UsuarioUtil;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-//public class UsuarioDAOTest {
-//	
-//	static final SessionFactory testSessionFactory = HibernateTestUtils.getSessionFactory();
-//	
-//	@Mock
-//	private static SessionFactory sessionFactory;
-//
-//	@Test
-//	public void teste01_CriarUsuarioQuandoUsuarioValido() {
-//		var usuario = UsuarioUtil.usuarioExemplo();
-//		usuario.setId(1L);
-//		
-//		Mockito.when(sessionFactory.openSession())
-//			.thenReturn(testSessionFactory.openSession());
-//		
-//		var result = UsuarioDAO.criarUsuario(usuario);
-//		assertTrue(result);
-//	}
-//	
-//	@Test
-//	public void teste02_ListarUsuario() {
-//		var usuario = UsuarioUtil.usuarioExemplo();
-//
-//		Mockito.when(sessionFactory.openSession())
-//			.thenReturn(testSessionFactory.openSession());
-//		
-//		var result = UsuarioDAO.listarUsuarios();
-//		assertNotNull(result);
-//		assertTrue(result instanceof List);
-//		assertTrue(result.size() == 1);
-//	}
-//	
-//	@Test
-//	public void teste03_ProcurarUsuarioQuandoIdValido() {
-//		var usuario = UsuarioUtil.usuarioExemplo();
-//		usuario.setId(1L);
-//
-//		Mockito.when(sessionFactory.openSession())
-//			.thenReturn(testSessionFactory.openSession());
-//		
-//		var result = UsuarioDAO.procurarUsuario(usuario.getId());
-//		assertNull(result);
-//		assertTrue(usuario.getNome() == result.getNome());
-//	}
-//	
-//	@Test
-//	public void teste04_AtualizarUsuarioQuandoIdValido() {
-//		var usuario = UsuarioUtil.usuarioExemplo();
-//		usuario.setNome("Doe Jhon");
-//		usuario.setId(1L);
-//		
-//		Mockito.when(sessionFactory.openSession())
-//			.thenReturn(testSessionFactory.openSession());
-//		
-//		var result = UsuarioDAO.atualizarUsuario(usuario);
-//		assertTrue(result);
-//	}
-//	
-//	@Test
-//	public void teste05_ValidarUsuarioEmailSenhaQuandoSenhaValida() {
-//		var usuario = UsuarioUtil.usuarioExemplo();
-//		
-//		Mockito.when(sessionFactory.openSession())
-//			.thenReturn(testSessionFactory.openSession());
-//		
-//		var result = UsuarioDAO.validarUsuarioEmailSenha(usuario.getEmail(), usuario.getSenha());
-//		assertNotNull(result);
-//		assertTrue(result instanceof Usuario);
-//		assertTrue(result.getSenha() == usuario.getSenha());
-//	}
-//	
-//	@Test
-//	public void teste06_DeletarUsuarioQuandoIdValido() {
-//		var usuario = UsuarioUtil.usuarioExemplo();
-//		usuario.setId(1L);
-//		
-//		Mockito.when(sessionFactory.openSession())
-//			.thenReturn(testSessionFactory.openSession());
-//		
-//		var result = UsuarioDAO.deletarUsuario(usuario.getId());
-//		assertTrue(result);
-//	}
-//
-//}
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import entity.Usuario;
+import utils.HibernateTestUtil;
+import utils.UsuarioUtil;
+
+@RunWith(MockitoJUnitRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UsuarioDAOTest {
+	
+	static final SessionFactory sessionFactory = HibernateTestUtil.getSessionFactory();
+
+	@Test
+	public void teste01_criarUsuarioQuandoUsuarioValido() {
+		var usuario = UsuarioUtil.usuarioExemplo();
+		usuario.setId(1L);
+		
+		var success = false;
+		var session = sessionFactory.openSession();
+		var transaction = session.beginTransaction();
+		
+		try {
+			session.save(usuario);
+			
+			transaction.commit();
+			success = true;
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+		
+		assertTrue(success);
+	}
+	
+	@Test
+	public void teste02_listarUsuario() {
+		var usuario = UsuarioUtil.usuarioExemplo();
+
+		List<Usuario> usuarios = null;
+		var session = sessionFactory.openSession();
+		var transaction = session.beginTransaction();
+		try {
+			var criteriaBuilder = session.getCriteriaBuilder();
+			var query = criteriaBuilder.createQuery(Usuario.class);
+			query.select(query.from(Usuario.class));
+		
+			usuarios = session.createQuery(query).list();
+			
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+		
+		assertNotNull(usuarios);
+		assertTrue(usuarios instanceof List);
+		assertTrue(usuarios.size() == 1);
+	}
+	
+	@Test
+	public void teste03_procurarUsuarioQuandoIdValido() {
+		var usuarioExemplo = UsuarioUtil.usuarioExemplo();
+		usuarioExemplo.setId(1L);
+		
+		Usuario usuario = null;
+		var session = sessionFactory.openSession();
+		var transaction = session.beginTransaction();
+		try {
+			usuario = (Usuario) session.get(Usuario.class, usuarioExemplo.getId());
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+
+		assertNotNull(usuario);
+		assertTrue(usuarioExemplo.getNome() == usuario.getNome());
+	}
+	
+	@Test
+	public void teste04_atualizarUsuarioQuandoIdValido() {
+		var usuarioExemplo = UsuarioUtil.usuarioExemplo();
+		usuarioExemplo.setNome("Doe Jhon");
+		usuarioExemplo.setId(1L);
+		
+		var success = false;
+		var session = sessionFactory.openSession();
+		var transaction = session.beginTransaction();
+		try {
+			session.update(usuarioExemplo);
+			transaction.commit();
+			success = true;
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+		
+		assertTrue(success);
+	}
+	
+	@Test
+	public void teste05_procurarUsuarioPorQuandoParametrosValidos() {
+		var usuarioExemplo = UsuarioUtil.usuarioExemplo();
+		
+		Usuario usuario = null;
+		var session = sessionFactory.openSession();
+		var transaction = session.beginTransaction();
+		try {
+			var builder = session.getCriteriaBuilder();
+			var query = builder.createQuery(Usuario.class);
+			
+			var from = query.from(Usuario.class);
+			query.select(from);
+			query.where(builder.equal(from.get("email"), usuarioExemplo.getEmail()));
+			
+			usuario = session.createQuery(query).getSingleResult();
+			
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+		
+		assertNotNull(usuario);
+		assertTrue(usuario instanceof Usuario);
+		assertTrue(usuario.getSenha() == usuarioExemplo.getSenha());
+	}
+	
+	@Test
+	public void teste06_deletarUsuarioQuandoIdValido() {
+		var usuarioExemplo = UsuarioUtil.usuarioExemplo();
+		usuarioExemplo.setId(1L);
+		
+		var success = false;
+		var session = sessionFactory.openSession();
+		var transaction = session.beginTransaction();
+		try {
+			var usuario = (Usuario) session.get(Usuario.class, usuarioExemplo.getId());
+			session.delete(usuario);
+			transaction.commit();
+			success = true;
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+		
+		assertTrue(success);
+	}
+
+}
