8b32ed7303789b7d834ab2de7aeb78f62741091f
Daniel Jr <danielmarcelo.junior@hotmail.com>
2021-12-13 02:21:05 -0300

    :bug: Corrige cadastro de produto
    Signed-off-by: Daniel Jr <danielmarcelo.junior@hotmail.com>

---
+++ src/main/java/servlet/ProdutoServlet.java
@@ -0,0 +1,86 @@
+package servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import entity.Categoria;
+import entity.Imagem;
+import entity.Produto;
+import entity.dao.CategoriaDAO;
+import entity.dao.ProdutoDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+/**
+ * Servlet implementation class ProdutoServlet
+ */
+@MultipartConfig
+public class ProdutoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ProdutoServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = (String) request.getParameter("nomeInput");
+		String descricao = (String) request.getParameter("descInput");
+		
+		String categoriaId = (String) request.getParameter("selectInput");
+		Categoria categoria = CategoriaDAO.procurarCategoria(Long.parseLong(categoriaId));
+		
+		Integer quantidade = null;
+		Double preco = null;
+		try {
+			quantidade = Integer.parseInt(request.getParameter("quantidadeInput"));
+			preco = Double.parseDouble(request.getParameter("preco"));
+		} catch (Exception e) {
+			e.printStackTrace();		
+		}
+		
+		Part imagemInputPart = request.getPart("imagemInput");
+		File file = File.createTempFile("imagem", LocalDateTime.now().toString());
+		imagemInputPart.write(file.getPath());
+		
+		Imagem imagem = new Imagem(
+			imagemInputPart.getSubmittedFileName(), 
+			imagemInputPart.getContentType(), 
+			Files.readAllBytes(file.toPath())
+		);
+		
+		Produto produto = new Produto(descricao, nome, quantidade, preco, categoria, imagem);
+		
+		boolean success;
+		try {
+			success = ProdutoDAO.cadastrarProduto(produto);
+		} catch (Exception e) {
+			success = false;
+		}
+		
+		if (success) {
+			String msgSuccess = "Produto cadastrado com sucesso";
+			response.sendRedirect("index.jsp?success=" + msgSuccess);
+		} else {
+			String msgError = "Erro ao cadastrar o produto";
+			String referer = request.getHeader("Referer");
+			response.sendRedirect(referer + "?error=" + msgError);
+		}
+	}
+
+}
--- src/main/webapp/anunciar.jsp
+++ src/main/webapp/anunciar.jsp
@@ -92,8 +92,18 @@
 		%>
 		<div class="p-3 my-3 mx-auto"
 			style="background-color: #c8d9ed; border-radius: 15px; max-width: 1000px">
+			<%
+			String error = request.getParameter("error");
+			if (error != null) {
+				%>
+				<div class="alert alert-danger" role="alert">
+				  <%= error %>
+				</div>
+				<%
+			}
+			%>
 			<form id="formularioAnunciar"
-				action="./processamentos/ProcessarAnunciar.jsp" method="post"
+				action="ProdutoServlet" method="post" enctype="multipart/form-data"
 				class="row g-3 justify-content-center mx-auto">
 
 				<div class="text-center">
@@ -272,17 +282,7 @@
 		</div>
 		<%
 		} else {
-		%>
-
-		<div class="card w-50 text-center top-50 start-50 translate-middle"
-			style="margin-top: 50px; background-color: silver;">
-			<div class="card-body">
-				<h5 class="card-title">Precisa entrar primeiro!</h5>
-				<button onclick="history.back()" class="btn btn-primary">voltar</button>
-			</div>
-		</div>
-
-		<%
+			response.sendRedirect("login.jsp");
 		}
 		%>
 
